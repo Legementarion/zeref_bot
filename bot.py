906929741c3ee0f60940fcee9e4f1630c793b5b6
Legementarion <mr.re66it@gmail.com>
2019-03-25 01:33:18 +0200

    send imgs to discord channel

--- bot.py
+++ bot.py
@@ -1,17 +1,34 @@
 import config
 import telebot
+import requests
+import json
 
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+@bot.channel_post_handler(content_types=['text', 'audio', 'document', 'photo', 'video'])
+def post_handler(message):
+    # sending get request and saving the response as response object
+    # response = requests.get(url=config.telegram_url, params=params)
 
+    # extracting data in json format
+    response = requests.get('http://api.telegram.org/bot{}/getFile?file_id={}'
+                            .format(config.token, message.photo[-1]
+                                    .file_id))
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        file_url = config.telegram_url + data['result']['file_path']
+
+        new_data = '''{
+            "embeds": [{
+                "image": {
+                    "url": "''' + file_url + '''"
+                }
+            }]
+        }'''
+        headers = {'Content-type': 'application/json'}
+        requests.post(config.discord_web_hook, data=new_data, headers=headers)
 
 
 bot.polling()
--- config.py
+++ config.py
@@ -1,1 +1,4 @@
 token = '741416679:AAHKNIwZfCSHJAePaQcsOKoaniDycAKjZiY'
+telegram_url = "https://api.telegram.org/file/bot" + token + "/"
+discord_web_hook = 'https://discordapp.com/api/webhooks/559483661490061323' \
+                   '/3jh6XUYtNjzhkWJ3557shba2lGnZeLuf0giZt2unWeb0RAxZouXV5vk8WSI16hKfpPYu'
