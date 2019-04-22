3999c40bc1b6446efc903164d6f9393339697872
Legementarion <mr.re66it@gmail.com>
2019-04-22 12:34:43 +0300

    update from img link to download up to discord

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
+from io import BytesIO
+
+from PIL import Image
 import config
 import telebot
-import requests
 import json
+import requests
+import urllib.request
+import tempfile
+
+from dhooks import Webhook, File
+
+hook = Webhook(config.discord_web_hook)
 
 bot = telebot.TeleBot(config.token)
 
 
 @bot.channel_post_handler(content_types=['text', 'audio', 'document', 'photo', 'video'])
 def post_handler(message):
-    # sending get request and saving the response as response object
-    # response = requests.get(url=config.telegram_url, params=params)
-
-    # extracting data in json format
     response = requests.get('http://api.telegram.org/bot{}/getFile?file_id={}'
                             .format(config.token, message.photo[-1]
                                     .file_id))
@@ -19,16 +24,15 @@ def post_handler(message):
     if response.status_code == 200:
         data = json.loads(response.text)
         file_url = config.telegram_url + data['result']['file_path']
+        fd = urllib.request.urlopen(file_url)
+        img = Image.open(BytesIO(fd.read()))
 
-        new_data = '''{
-            "embeds": [{
-                "image": {
-                    "url": "''' + file_url + '''"
-                }
-            }]
-        }'''
-        headers = {'Content-type': 'application/json'}
-        requests.post(config.discord_web_hook, data=new_data, headers=headers)
+        tmpfile = tempfile.TemporaryFile()
+        img.save(tmpfile, "PNG")
+        tmpfile.seek(0)
+        file = File(tmpfile, name='bluepost.png')  # optional name for discord
+
+        hook.send('Look at this:', file=file)
 
 
 bot.polling()
